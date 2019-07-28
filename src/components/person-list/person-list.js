a9b65804efa5fd81fd00b65763008f192199e95a
Vladimir Kochenov <kochenov.vl@gmail.com>
2019-07-28 17:30:42 +0300

    Update styles for person-list

--- src/components/person-list/person-list.js
+++ src/components/person-list/person-list.js
@@ -6,27 +6,29 @@ import './person-list.css'
 
 const PersonList = ({ people, onPersonSelected }) => {  
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Имя</th>
-          <th>Фамилия</th>
-          <th>Должность</th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-          people.map((person) => {
-            return (
-              <tr key={person.id}
-                  onClick={() => { onPersonSelected(person.id) }}>
-                <PersonListItem person={person} />
-              </tr>
-            )
-          })
-        }
-      </tbody>
-    </table>
+    <div className="table-responsive mt-4 person-list">
+      <table className="table table-dark table-hover">
+        <thead>
+          <tr>
+            <th>Имя</th>
+            <th>Фамилия</th>
+            <th>Должность</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            people.map((person) => {
+              return (
+                <tr key={person.id}
+                    onClick={() => { onPersonSelected(person.id) }}>
+                  <PersonListItem person={person} />
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
+    </div>
   )
 }
 
